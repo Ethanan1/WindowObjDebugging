7f2fa312309ddc9be44914c54146cc306f305e9e
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2022-08-16 21:55:34 -0700

    manipulate

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,13 +1,16 @@
 export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
-    
+
     // Your code here
+    document.title = "Ethan Kaur's Portfolio";
 }
 
 export function changeHeader() {
     // Change the name in the h1 of the page to your name
 
     // Your code here
+    document.querySelector('h1').innerHTML = "I am Ethan Kaur";
+    // document.body.children[0].innerHTNL = "I am Ethan Kaurs";
 }
 
 export function changeAboutMe() {
@@ -15,4 +18,6 @@ export function changeAboutMe() {
      passage about yourself */
 
     // Your code here
+    document.querySelector(".section h2 + p").innerHTML = "Foobar";
+    document.querySelector(".section h2 + p + p").innerHTML = "Foobar2";
 }
